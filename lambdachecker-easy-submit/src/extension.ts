383138baadd839465f9b73729a53c3aaccd69510
m-eduard <marineduardconstantin2001@gmail.com>
2024-05-04 01:13:52 +0300

    Format code with ESLint

--- lambdachecker-easy-submit/src/extension.ts
+++ lambdachecker-easy-submit/src/extension.ts
@@ -1,109 +1,143 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
-import { LambdaChecker } from './lambdaChecker';
-import { Storage } from './storage';
-import StatusBar from './ui/statusBar';
-import ContestDataProvider from './ui/fileSystem/contestDataProvider';
+import * as vscode from "vscode";
+import { LambdaChecker } from "./lambdaChecker";
+import { Storage } from "./storage";
+import StatusBar from "./ui/statusBar";
+import ContestDataProvider from "./ui/fileSystem/contestDataProvider";
+import ProblemDataProvider from "./ui/fileSystem/problemDataProvider";
 
 interface ProblemProps {
-	title: string;
-	description: string,
-	skel: string
+  title: string;
+  description: string;
+  skel: string;
 }
 
 export async function activate(context: vscode.ExtensionContext) {
-	Storage.setContext(context);
+  Storage.setContext(context);
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "lambdachecker-easy-submit" is now active!');
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log(
+    'Congratulations, your extension "lambdachecker-easy-submit" is now active!'
+  );
 
-	let disposable = vscode.commands.registerCommand('lambdachecker-easy-submit.helloWorld', () => {
-		vscode.window.showInformationMessage('Hello World from LambdaChecker!');
+  let disposable = vscode.commands.registerCommand(
+    "lambdachecker-easy-submit.helloWorld",
+    () => {
+      vscode.window.showInformationMessage("Hello World from LambdaChecker!");
 
-		vscode.window.registerFileDecorationProvider({
-			provideFileDecoration: (uri: vscode.Uri) => {
-				const isFile = uri.scheme === 'file';
-				return {
-					badge: isFile ? '📁' : undefined,
-					tooltip: isFile ? 'File' : undefined
-				};
-			}
-		});
-	});
+      vscode.window.registerFileDecorationProvider({
+        provideFileDecoration: (uri: vscode.Uri) => {
+          const isFile = uri.scheme === "file";
+          return {
+            badge: isFile ? "📁" : undefined,
+            tooltip: isFile ? "File" : undefined,
+          };
+        },
+      });
+    }
+  );
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 
-	let submitCmd = vscode.commands.registerCommand('lambdachecker.submit', () => {
-		vscode.window.showInformationMessage('Submitting the problem!');
-	});
+  let submitCmd = vscode.commands.registerCommand(
+    "lambdachecker.submit",
+    () => {
+      vscode.window.showInformationMessage("Submitting the problem!");
+    }
+  );
 
-	context.subscriptions.push(submitCmd);
+  context.subscriptions.push(submitCmd);
 
-	context.subscriptions.push(
-		vscode.commands.registerCommand('lambdachecker.login', () => {
-			LambdaChecker.loginUi();
-			LambdaChecker.contestsUi();
-		})
-	);
+  context.subscriptions.push(
+    vscode.commands.registerCommand("lambdachecker.login", () => {
+      LambdaChecker.loginUi();
+      LambdaChecker.contestsUi();
+    })
+  );
 
-	context.subscriptions.push(StatusBar.statusBarItem);
-	StatusBar.updateStatus();
+  context.subscriptions.push(StatusBar.statusBarItem);
+  StatusBar.updateStatus();
 
-	const loggedInUsername = await LambdaChecker.getLoginStatus();
+  const loggedInUsername = await LambdaChecker.getLoginStatus();
 
-	if (loggedInUsername !== undefined) {
-		vscode.window.showInformationMessage("Already logged in!");
-		vscode.window.registerTreeDataProvider('lambdachecker.problems', new TreeDataProvider());
+  if (loggedInUsername !== undefined) {
+    vscode.window.showInformationMessage("Already logged in!");
+    vscode.window.registerTreeDataProvider(
+      "lambdachecker.problems",
+      new TreeDataProvider()
+    );
 
-		StatusBar.updateStatus(loggedInUsername);
+    StatusBar.updateStatus(loggedInUsername);
 
-		vscode.window.createTreeView('lambdachecker.contests', {
-			treeDataProvider: new ContestDataProvider(LambdaChecker.client)
-		});
+    const contestsTreeView = vscode.window.createTreeView(
+      "lambdachecker.contests",
+      {
+        treeDataProvider: new ContestDataProvider(LambdaChecker.client),
+      }
+    );
 
-		// vscode.window.registerTreeDataProvider('lambdachecker.contests', new ContestDataProvider());
-	}
+    const problemsTreeView = vscode.window.createTreeView(
+      "lambdachecker.problems",
+      {
+        treeDataProvider: new ProblemDataProvider(LambdaChecker.client),
+      }
+    );
+
+    // vscode.window.registerTreeDataProvider('lambdachecker.contests', new ContestDataProvider());
+  }
 }
 
 class TreeDataProvider implements vscode.TreeDataProvider<TreeItem> {
-	onDidChangeTreeData?: vscode.Event<TreeItem|null|undefined>|undefined;
-  
-	data: TreeItem[];
-  
-	constructor() {
-	  this.data = [new TreeItem('cars', [
-		new TreeItem(
-			'Ford', [new TreeItem('Fiesta'), new TreeItem('Focus'), new TreeItem('Mustang')]),
-		new TreeItem(
-			'BMW', [new TreeItem('320'), new TreeItem('X3'), new TreeItem('X5')])
-	  ])];
-	}
-  
-	getTreeItem(element: TreeItem): vscode.TreeItem|Thenable<vscode.TreeItem> {
-	  return element;
-	}
-  
-	getChildren(element?: TreeItem|undefined): vscode.ProviderResult<TreeItem[]> {
-	  if (element === undefined) {
-		return this.data;
-	  }
-	  return element.children;
-	}
+  onDidChangeTreeData?: vscode.Event<TreeItem | null | undefined> | undefined;
+
+  data: TreeItem[];
+
+  constructor() {
+    this.data = [
+      new TreeItem("cars", [
+        new TreeItem("Ford", [
+          new TreeItem("Fiesta"),
+          new TreeItem("Focus"),
+          new TreeItem("Mustang"),
+        ]),
+        new TreeItem("BMW", [
+          new TreeItem("320"),
+          new TreeItem("X3"),
+          new TreeItem("X5"),
+        ]),
+      ]),
+    ];
   }
-  
-  class TreeItem extends vscode.TreeItem {
-	children: TreeItem[]|undefined;
-  
-	constructor(label: string, children?: TreeItem[]) {
-	  super(
-		  label,
-		  children === undefined ? vscode.TreeItemCollapsibleState.None :
-								   vscode.TreeItemCollapsibleState.Expanded);
-	  this.children = children;
-	}
+
+  getTreeItem(element: TreeItem): vscode.TreeItem | Thenable<vscode.TreeItem> {
+    return element;
   }
+
+  getChildren(
+    element?: TreeItem | undefined
+  ): vscode.ProviderResult<TreeItem[]> {
+    if (element === undefined) {
+      return this.data;
+    }
+    return element.children;
+  }
+}
+
+class TreeItem extends vscode.TreeItem {
+  children: TreeItem[] | undefined;
+
+  constructor(label: string, children?: TreeItem[]) {
+    super(
+      label,
+      children === undefined
+        ? vscode.TreeItemCollapsibleState.None
+        : vscode.TreeItemCollapsibleState.Expanded
+    );
+    this.children = children;
+  }
+}
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
--- lambdachecker-easy-submit/src/ui/statusBar.ts
+++ lambdachecker-easy-submit/src/ui/statusBar.ts
@@ -1,19 +1,20 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 export default class StatusBar {
-    static readonly statusBarItem =
-        vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left);
-    
-    static {
-        this.statusBarItem.command = 'lambdachecker.login';
-        this.statusBarItem.show();
-    }
+  static readonly statusBarItem = vscode.window.createStatusBarItem(
+    vscode.StatusBarAlignment.Left
+  );
 
-    public static updateStatus(username?: string) {
-        if (username !== undefined) {
-            this.statusBarItem.text = `\u03BB Checker: ${username}`;
-        } else {
-            this.statusBarItem.text = '\u03BB Checker: Sign in';
-        }
+  static {
+    this.statusBarItem.command = "lambdachecker.login";
+    this.statusBarItem.show();
+  }
+
+  public static updateStatus(username?: string) {
+    if (username !== undefined) {
+      this.statusBarItem.text = `\u03BB Checker: ${username}`;
+    } else {
+      this.statusBarItem.text = "\u03BB Checker: Sign in";
     }
+  }
 }
